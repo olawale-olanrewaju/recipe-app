c216e35bc576fc0999297804d60fe0a84a4f0b03
Olawale Olanrewaju <olanrewajume@yahoo.com>
2021-01-01 12:57:13 +0000

    Added wait_for_db function

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,11 @@ MAINTAINER Olawale Olanrewaju
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-dev \
+        gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r /requirements.txt
+RUN apk del .tmp-build-dev
 
 RUN mkdir /app
 WORKDIR /app
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,41 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@email.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@email.com',
+            password='password123',
+            name='Test User'
+        )
+
+    def test_user_listed(self):
+        """Test users are listed on admin page"""
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.email)
+        self.assertContains(response, self.user.name)
+
+    def test_user_change_page(self):
+        """Test that tghe user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_psge(self):
+        """Test that create user page works"""
+        url = reverse('admin:core_user_add')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
